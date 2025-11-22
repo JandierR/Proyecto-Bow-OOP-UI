505e662ed0883449962bb08040e36f10699974af
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2025-11-22 13:15:35 -0600

    Detalle domain

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"cr.ac.ucenfotec.rojas.jandier.tl"},{"l":"cr.ac.ucenfotec.rojas.jandier.UI"}];updateSearchResults();
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"cr.ac.ucenfotec.rojas.jandier.tl","l":"Controller"},{"p":"<Unnamed>","l":"Main"},{"p":"cr.ac.ucenfotec.rojas.jandier.UI","l":"UI"}];updateSearchResults();
